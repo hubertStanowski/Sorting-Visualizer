88e0f68d640e8919814f3d4909c4bf6c550615c1
Hubert <hubert.stanowski@gmail.com>
2021-03-08 18:53:32 +0100

    Add the quick sort algorithm with randomizing the pivot (not visualization)

--- Quick Sort/quick_sort.py
+++ Quick Sort/quick_sort.py
@@ -1,23 +1,32 @@
-def partition(array, p, r):
-    q = p
-    pivot = array[r]
+import random
 
-    for j in range(p, r):
+
+def partition(array, p, r):
+    q = p + 1
+    pivot = array[p]
+    for j in range(p + 1, r + 1):
 
         if array[j] <= pivot:
             array[q], array[j] = array[j], array[q]
             q += 1
 
-    array[q], array[r] = array[r], array[q]
+    array[q - 1], array[p] = array[p], array[q - 1]
 
-    return q
+    return q - 1
+
+
+def random_partition(array, p, r):
+    pivot = random.randrange(p, r)
+    array[p], array[pivot] = array[pivot], array[p]
+    return partition(array, p, r)
 
 
 def quick_sort(array, p, r):
     if len(array) < 2:
         return array
+        
     if p < r:
-        q = partition(array, p, r)
+        q = random_partition(array, p, r)
         quick_sort(array, p, q - 1)
         quick_sort(array, q + 1, r)
         return array
