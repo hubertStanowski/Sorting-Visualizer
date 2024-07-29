bab9b6eff2f984b834442f934760f6e0f6f45cd3
Hubert Stanowski <hubert.stanowski@gmail.com>
2024-07-29 17:44:08 +0200

    Add constants to the cleaner redo

---
+++ source/constants.py
@@ -0,0 +1,21 @@
+WINDOW_WIDTH, WINDOW_HEIGHT = 1500, 950
+TOP_BAR = 250
+BOTTOM_BAR = 50
+SIDE_BAR = 50
+BUTTON_WIDTH, BUTTON_HEIGHT = 150, 50
+FONT = None
+
+max_value = None
+animation_speed = "F"
+size = "M"
+delay = 0
+
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+
+SELECTION_COLOR = (186, 85, 211)
