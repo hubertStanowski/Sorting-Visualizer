d28bae7cf04de97da4d2ed5df23fadff5ad9a602
Hubert Stanowski <hubert.stanowski@gmail.com>
2024-07-29 18:11:41 +0200

    Add skeleton for the main program

---
+++ source/main.py
@@ -0,0 +1,26 @@
+from constants import *
+from screen import initialize_screen
+
+import pygame
+
+
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Sorting Algorithms Visualizer")
+
+    screen = initialize_screen(window)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 0
+
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+
+        screen.draw()
+
+
+if __name__ == "__main__":
+    main()
